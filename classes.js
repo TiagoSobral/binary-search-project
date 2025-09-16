a2529da966673d9696cfb65001ba502247e7ddcc
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-09-16 16:15:31 -0400

    (remove) classes file.
    -> classes and functions are together.

--- classes.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-export function Node(data, left, right) {
-	return { data, left, right };
-}
-
-export function Tree(array = []) {
-	const root = buildTree(array);
-	return { array, root };
-}
