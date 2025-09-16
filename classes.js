fcbe8c84c018f63c2d46ec14af67191354f46010
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-09-15 21:33:13 -0400

    (feat) add functions Node & Tree.

--- classes.js
+++ classes.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+export function Node(data, left, right) {
+	return { data, left, right };
+}
+
+export function Tree(array = []) {
+	const root = buildTree(array);
+	return { array, root };
+}
