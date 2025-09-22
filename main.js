b84809db43b72f2be5ce572f9b1da9c299b1743c
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-09-22 19:37:41 -0400

    (feat) add testing

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 #!/usr/bin/node
-import { prettyPrint, Tree } from './functions.js';
+import { getRandomArray, prettyPrint, Tree } from './functions.js';
 
-const binaryTree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-
-prettyPrint(binaryTree.root);
+const test = Tree(getRandomArray());
+prettyPrint(test.root);
+console.log(test.isBalanced());
+test.levelOrderForEach(console.log);
+test.preOrderForEach(console.log);
+test.postOrderForEach(console.log);
+test.inOrderForEach(console.log);
+test.insert(103);
+test.insert(189);
+test.insert(312);
+test.insert(200);
+test.insert(123);
+test.insert(150);
+prettyPrint(test.root);
+console.log(test.isBalanced());
+test.rebalance();
+prettyPrint(test.root);
+console.log(test.isBalanced());
+test.levelOrderForEach(console.log);
+test.preOrderForEach(console.log);
+test.postOrderForEach(console.log);
+test.inOrderForEach(console.log);
